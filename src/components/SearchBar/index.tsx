161098bdba2f0d4b90c0b1075097c3afb4bbbb39
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-06-13 08:57:57 -0300

    search bar css fixes

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -3,9 +3,8 @@ import {
   Theme,
   makeStyles,
 } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
 import { alpha } from '@material-ui/core/styles';
+import { OutlinedInput } from '@mui/material';
 import { useState } from 'react';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -30,21 +29,11 @@ const useStyles = makeStyles((theme: Theme) =>
         width: 'auto',
       },
     },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
     inputRoot: {
       color: 'inherit',
     },
     inputInput: {
       padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
       paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
       transition: theme.transitions.create('width'),
       width: '100%',
@@ -63,16 +52,13 @@ export default function SearchBar({ handleSearch }) {
   const classes = useStyles();
   return (
     <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon />
-      </div>
       <form
         onSubmit={(event) => {
           event.preventDefault();
           handleSearch(searchValue);
         }}
       >
-        <InputBase
+        <OutlinedInput
           type="text"
           onChange={(event) =>
             setSearchValue(event.target.value)
